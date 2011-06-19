106cb3d2282a8f61ff15e864dd8855e0f5d0e239
Swizec Teller <swizec@swizec.com>
2011-06-19 23:47:17 +0200

    tweaks for accepting more general text

--- syllabification/run.clj
+++ syllabification/run.clj
@@ -2,11 +2,26 @@
 (ns syllabification
   (:require [clojure.string :as str]))
 
-(def text "this is a sample text")
+(def text (str/lower-case
+"Oft have we trod the vales of Castaly
+And heard sweet notes of sylvan music blown
+From antique reeds to common folk unknown:
+And often launched our bark upon that sea
+Which the nine Muses hold in empery,
+And ploughed free furrows through the wave and foam,
+Nor spread reluctant sail for more safe home
+Till we had freighted well our argosy.
+
+Of which despoiled treasures these remain,
+Sordello's passion, and the honied line
+Of young Endymion, lordly Tamburlaine
+Driving his pampered jades, and more than these,
+The seven-fold vision of the Florentine,
+And grave-browed Milton's solemn harmonies."))
 
 (defn transform [text]
   (map #(if (= %1 " ") " "
             (if (re-matches #"[aeiou]" %1) "V" "C"))
-            (str/split text #"")))
+            (filter #(re-matches #"[a-z ]" %1) (str/split text #""))))
 
 (println (transform text))
